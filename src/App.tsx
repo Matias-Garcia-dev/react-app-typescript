572971af301205a7f42c86f02dbe1106948cda82
Matias <matiasenriquegarciadev@gmail.com>
2023-10-10 22:28:56 -0300

    updated logo and nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import HomeScreen from './components/homeScreen/HomeScreen';
 
 function App() {
   return (
     <div >
+      <HomeScreen></HomeScreen>
     </div>
   );
 }
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158" alt=""/>
+        Nav
+    </div>
+  )
+}
+
+export default Nav
