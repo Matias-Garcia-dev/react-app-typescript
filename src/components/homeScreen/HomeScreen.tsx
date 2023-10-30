ad20299247978f32d2b157b8ee4f49559ac506b4
Matias <matiasenriquegarciadev@gmail.com>
2023-10-30 12:13:42 -0300

    Updated Nav

--- src/components/homeScreen/HomeScreen.tsx
+++ src/components/homeScreen/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './HomeScreen.css'
 import Nav from '../nav/Nav'
+import Banner from '../banner/Banner'
 
 function HomeScreen() {
   return (
     <div className='homeScreen'>
       <Nav></Nav>
+      <Banner></Banner>
     </div>
   )
 }
