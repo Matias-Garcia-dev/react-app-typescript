58b74e2f4dc2dc3b91ae2c9f37572bdb30125dbb
Matias <matiasenriquegarciadev@gmail.com>
2023-11-02 19:00:24 -0300

    deployed in firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdisihqZqGnEXrMUj_-LPuji9EnfPj4gk",
+    authDomain: "netflix-clone-app-f4e53.firebaseapp.com",
+    projectId: "netflix-clone-app-f4e53",
+    storageBucket: "netflix-clone-app-f4e53.appspot.com",
+    messagingSenderId: "654611395407",
+    appId: "1:654611395407:web:ed67450591158fbcc04b3a"
+  };
