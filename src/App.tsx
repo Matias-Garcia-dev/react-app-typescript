2532541ee82543c49b90d8249fe23a791412fd2f
Matias <matiasenriquegarciadev@gmail.com>
2023-11-04 16:09:13 -0300

    Update loginScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import HomeScreen from './components/homeScreen/HomeScreen';
+import HomeScreen from './components/screens/homeScreen/HomeScreen';
 import './App.css'
+import LoginScreen from './components/screens/homeScreen/LoginScreen';
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className='app'>
-      <HomeScreen></HomeScreen>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen></HomeScreen>}/>
+          <Route path="/login" element={<LoginScreen/>}/>
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
